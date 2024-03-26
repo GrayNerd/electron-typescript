60754f02c2a7564f27c728a9e3bf1aed284a72bc
Ken Boyd <the.other.ken@gmail.com>
2024-03-25 23:34:00 -0600

    Converted to modules and added example usage of ipcMain and ipcRenderer

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,32 @@
-const {app, BrowserWindow} = require('electron');
-console.log('Starting app');
+// const {app, BrowserWindow, ipcMain} = require('electron');
+import { app, BrowserWindow, ipcMain } from 'electron';
 
 app.on('ready', () => {
-  console.log('App is ready');
-
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
-    }
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+    show: false,
   });
-  win.loadURL(`file://${__dirname}/index.html`);
+
+  const __dirname = import.meta.dirname;
+  win.loadURL(`file:///home/ken/src/electron-typescript/build/index.html`);
   win.webContents.openDevTools();
+
+  win.on('closed', () => {
+    win.destroy();
+  });
+
+  win.on('ready-to-show', () => {
+    win.show();
+    win.webContents.send('new-main-message', 'This is a message from the main process');
+  });
+});
+
+ipcMain.on('new-renderer-message', (event: any, message: string) => {
+  console.log(message);
+  event.reply('replying-main-message', 'This is a reply to the message from the renderer process');
 });
--- src/renderer.ts
+++ src/renderer.ts
@@ -1,1 +1,18 @@
-document.body.innerHTML += "WooHoo, it works!";
+const { ipcRenderer } = require('electron');
+
+document.body.innerHTML += "<br>WooHoo, it works!";
+
+ipcRenderer.on(
+    "new-main-message",
+    (event: any, message: string) => {
+        document.body.innerHTML += `<br><br><br>${message}`;
+        ipcRenderer.send("new-renderer-message", "This is a message from the renderer process");
+    }
+);
+
+ipcRenderer.on(
+    "replying-main-message",
+    (event: any, message: string) => {
+        document.body.innerHTML += `<br><br><br>${message}`;
+    }
+);
